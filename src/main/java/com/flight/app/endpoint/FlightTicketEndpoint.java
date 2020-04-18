acc490d3b82f9fb538a2620d3dc06568d6fd05df
Ramiro Alarcon <rramiro88@gmail.com>
2020-04-18 17:28:03 -0300

    Added swagger ui. Also added postman collection.

--- src/main/java/com/flight/app/endpoint/FlightTicketEndpoint.java
+++ src/main/java/com/flight/app/endpoint/FlightTicketEndpoint.java
@@ -4,15 +4,23 @@ import com.flight.app.converter.FlightTicketConverter;
 import com.flight.app.dto.FlightTicketDto;
 import com.flight.app.entity.FlightTicket;
 import com.flight.app.service.FlightTicketService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ticket")
 @AllArgsConstructor
+@Api(
+        tags = "FlightTicketsApi",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+)
 public class FlightTicketEndpoint {
 
     @Autowired
@@ -22,6 +30,7 @@ public class FlightTicketEndpoint {
     private FlightTicketConverter converter;
 
     @GetMapping(value = "/{id}")
+    @ApiOperation(value = "Finds a ticket", nickname = "findTicket")
     public ResponseEntity findTicket(@PathVariable("id") Long id){
 
         try{
@@ -40,6 +49,7 @@ public class FlightTicketEndpoint {
     }
 
     @PostMapping
+    @ApiOperation(value = "Creates a new ticket", nickname = "saveTicket")
     public ResponseEntity createTicket(@RequestBody FlightTicketDto dto){
 
         try {
